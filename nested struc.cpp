cd5bcb849421f16e13225944c379dcb13f4e905f
girajalasainaidu-bit <girajalasainaidu@gmail.com>
2026-04-17 11:48:48 +0530

    Add files via upload

---
+++ nested struc.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+// Declaration of the
+// dependent structure
+struct Employee
+{
+int employee_id;
+char name[20];
+int salary;
+};
+// Declaration of the
+// Outer structure
+struct Organisation
+{
+char organisation_name[20];
+char org_number[20];
+// Dependent structure is used
+// as a member inside the main
+// structure for implementing
+// nested structure
+struct Employee emp;
+};
+// Driver code
+int main()
+{
+// Structure variable
+struct Organisation org;
+// Print the size of organisation
+// structure
+printf("The size of structure organisation : %ld\n", sizeof(org));
+org.emp.employee_id = 101;
+strcpy(org.emp.name, "Robert");
+org.emp.salary = 400000;
+strcpy(org.organisation_name, "Aditya University");
+strcpy(org.org_number, "ADTPPU");
+// Printing the details
+printf("Organisation Name : %s\n", org.organisation_name);
+printf("Organisation Number : %s\n", org.org_number);
+
+printf("Employee id : %d\n", org.emp.employee_id);
+printf("Employee name : %s\n", org.emp.name);
+printf("Employee Salary : %d\n", org.emp.salary);
+}
